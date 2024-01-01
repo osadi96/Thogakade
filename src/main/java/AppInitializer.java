d5adbd8ddfbef43f3d87f2f388f442a054071f72
osadi Chadeepani <osadichadeepani@gmail.com>
2024-01-01 21:21:54 +0530

    Create customer Button

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,7 +15,7 @@ public class AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/Dashboardform.fxml"))));
-
+        primaryStage.setTitle("MAINMENU");
         primaryStage.show();
     }
 }
